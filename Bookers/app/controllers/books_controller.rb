6ef740785dd94be9db2a00cbe61aafcf7e257524
sumi-re01 <owob.viola@gmail.com>
2021-02-03 08:28:02 +0000

    [Fix]サクセスメッセージ スペル修正

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was succesfully created."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book)
     else
       @books = Book.all
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Book was succesfully updated"
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book)
     else
       render :edit
@@ -39,7 +39,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "Book was succesfully destroyed"
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
