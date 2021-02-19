b6a869269c154b276eb44bb4503fe94e4651cdfb
sumi-re01 <owob.viola@gmail.com>
2021-02-19 12:59:13 +0000

    [Add]バッチ処理の記載

--- Bookers/lib/batch/data_reset.rb
+++ Bookers/lib/batch/data_reset.rb
@@ -4,3 +4,8 @@ class Batch::DataReset
     p "投稿を全て削除しました"
   end
 end
+# config/application.rbに読み込みの記載
+# config.paths.add 'lib', eager_load: true
+# ↓で実行
+# bundle exec rails runner Batch::DataReset.data_reset
+# 定期的な実行はcronで設定する
